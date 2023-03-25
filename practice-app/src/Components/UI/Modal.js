c5056594eaa13417d10ad7ef40706b8131555fa6
Omkar Patole <omkarpatole1799@gmail.com>
2023-03-25 15:22:46 +0530

    portal component

--- practice-app/src/Components/UI/Modal.js
+++ practice-app/src/Components/UI/Modal.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Button from './Button';
 import Card from './Card';
 import styles from './Modal.module.css';
-function Modal(props) {
+
+const Overlay = (props) => {
     return (
         <>
             <div
                 className={styles.backdrop}
                 onClick={props.onCloseModal}
             ></div>
+        </>
+    );
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <>
             <Card className={`${styles.modal}`}>
                 <header className={styles.header}>
                     <h2>{props.title}</h2>
@@ -22,6 +31,26 @@ function Modal(props) {
                     <Button onClick={props.onCloseModal}>Close</Button>
                 </footer>
             </Card>
+        </>
+    );
+};
+
+function Modal(props) {
+    return (
+        <>
+            {ReactDOM.createPortal(
+                <Overlay onCloseModal={props.onCloseModal} />,
+                document.getElementById('modal-overlay-root'),
+            )}
+
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onCloseModal={props.onCloseModal}
+                />,
+                document.getElementById('modal-box-root'),
+            )}
         </>
     );
 }
