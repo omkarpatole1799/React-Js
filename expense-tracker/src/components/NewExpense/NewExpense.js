879a3bf5eaa5b158416cdb41e19b53bc58d05df0
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-10 22:23:19 +0530

    init

--- expense-tracker/src/components/NewExpense/NewExpense.js
+++ expense-tracker/src/components/NewExpense/NewExpense.js
@@ -2,55 +2,56 @@ import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = (props) => {
-    const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const expenseHandler = (expenses) => {
-        const expense = {
-            ...expenses,
-            id: Math.random().toString(),
-        };
-        props.onAllExpenseData(expense);
+  const expenseHandler = (expenses) => {
+    const expense = {
+      ...expenses,
+      id: Math.random().toString(),
     };
+    props.onAllExpenseData(expense);
+  };
 
-    const editFormHandler = () => {
-        setIsEditing(true);
-    };
-    const cancelEditFormHandler = () => {
-        setIsEditing(false);
-    };
+  const editFormHandler = () => {
+    setIsEditing(true);
+  };
 
-    // method 1 //
-    // if (isEditing === false) {
-    //     return <div className='new_expenses__container'>
-    //         <button className='add_expense_btn' onClick={editFormHandler}> Add Expenses </button>
-    //     </div>
-    // }
+  const cancelEditFormHandler = () => {
+    setIsEditing(false);
+  };
 
-    // return (
-    //     <div className='new_expenses__container'>
+  // method 1 //
+  // if (isEditing === false) {
+  //     return <div className='new_expenses__container'>
+  //         <button className='add_expense_btn' onClick={editFormHandler}> Add Expenses </button>
+  //     </div>
+  // }
 
-    //         <ExpenseForm onAddExpense={expenseHandler} onCancel={cancelEditFormHandler}/>
+  // return (
+  //     <div className='new_expenses__container'>
 
-    //     </div>
-    // )
+  //         <ExpenseForm onAddExpense={expenseHandler} onCancel={cancelEditFormHandler}/>
 
-    // method 2 //
+  //     </div>
+  // )
 
-    return (
-        <div className="new_expenses__container">
-            {!isEditing && (
-                <button className="add_expense_btn" onClick={editFormHandler}>
-                    Add Expenses
-                </button>
-            )}
-            {isEditing && (
-                <ExpenseForm
-                    onAddExpense={expenseHandler}
-                    onCancel={cancelEditFormHandler}
-                />
-            )}
-        </div>
-    );
+  // method 2 //
+
+  return (
+    <div className="new_expenses__container">
+      {!isEditing && (
+        <button className="add_expense_btn" onClick={editFormHandler}>
+          Add Expenses
+        </button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onAddExpense={expenseHandler}
+          onCancel={cancelEditFormHandler}
+        />
+      )}
+    </div>
+  );
 };
 
 export default NewExpense;
