3a423c0bd63c3533a4c54f0599de5447adc112b6
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-16 10:03:25 +0530

    added useContext

--- login-system/src/Components/Header/Navbar.js
+++ login-system/src/Components/Header/Navbar.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import Button from '../UI/Button/Button'
-import classes from './Navbar.module.css'
-function Navbar(props) {
-    return (
-        <>
-            {props.isLoggedIn && (
-                <nav className={classes.nav}>
-                    <ul>
-                        <li>
-                            <a href="">HOME</a>
-                        </li>
-                        <li>
-                            <a href="">ABOUT</a>
-                        </li>
-                        <li>
-                            <button onClick={props.onLogout}>LOGOUT</button>
-                        </li>
-                    </ul>
-                </nav>
-            )}
-        </>
-    )
+import React, { useContext } from "react"
+import Button from "../UI/Button/Button"
+
+import classes from "./Navbar.module.css"
+import AuthContext from "../../store/auth-contex"
+function Navbar() {
+  const ctx = useContext(AuthContext)
+
+  return (
+    <>
+      {ctx.isLoggedIn && (
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <a href="">HOME</a>
+            </li>
+            <li>
+              <a href="">ABOUT</a>
+            </li>
+            <li>
+              <button onClick={ctx.onLogout}>LOGOUT</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
+  )
 }
 
 export default Navbar
