6cf767e1b2eecfefcc23c5c76e6229a6a88b028e
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-19 12:03:23 +0530

    refactoring

--- food-order-app/src/Components/Meals/Meals.js
+++ food-order-app/src/Components/Meals/Meals.js
@@ -8,28 +8,24 @@ const DUMMY_MEALS = [
     mealName: "Tomato",
     description: "Special fry",
     price: 20,
-    quantity: 0,
   },
   {
     id: "m2",
     mealName: "puran poli",
     description: "Indian special",
     price: 620,
-    quantity: 0,
   },
   {
     id: "m3",
     mealName: "Potato Fry",
     description: "Chipped potato fry",
     price: 100,
-    quantity: 0,
   },
   {
     id: "m4",
     mealName: "Veggiee Mix",
     description: "All veggies mix",
     price: 230,
-    quantity: 0,
   },
 ]
 
