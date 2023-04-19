d308218bf9a13033bfc2307d06d0ed207a333df3
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-19 08:44:32 +0530

    added cart item component

--- food-order-app/src/Components/Cart/Cart.js
+++ food-order-app/src/Components/Cart/Cart.js
@@ -2,9 +2,11 @@ import React from "react"
 import Button from "../UI/Button/Button"
 import Modal from "../UI/Modal"
 import classes from "./Cart.module.css"
-function Cart() {
+import CartItem from "./CartItem"
+function Cart(props) {
   const mealData = [
     {
+      id: "m1",
       mealName: "Tomato",
       price: 12.99,
       quantity: "2",
@@ -13,24 +15,10 @@ function Cart() {
   ]
 
   return (
-    <Modal>
+    <Modal onClick={props.onClick}>
       <ul className={classes.cart}>
         <div className={classes.cartItem}>
-          {mealData.map((meal) => {
-            return (
-              <li key={meal.id} className={classes.mealItem}>
-                <div className="meal-info">
-                  <p>{meal.mealName}</p>
-                  <p>{meal.description}</p>
-                  <p>$ {meal.price.toFixed(2)}</p>
-                </div>
-                <div className={classes.mealItemQuantityTotal}>
-                  <span>Quantity</span>
-                  <span> x {meal.quantity} </span>
-                </div>
-              </li>
-            )
-          })}
+          <CartItem items={mealData} />
         </div>
         <div className={classes.cartSummary}>
           <div className={classes.cartTotal}>
@@ -38,7 +26,9 @@ function Cart() {
             <span>$ 25.99</span>
           </div>
           <div className={classes.cartButton}>
-            <Button className={classes.cartCloseButton}>Close</Button>
+            <Button onClick={props.onClick} className={classes.cartCloseButton}>
+              Close
+            </Button>
             <Button className={classes.cartOrderButton}>Order</Button>
           </div>
         </div>
