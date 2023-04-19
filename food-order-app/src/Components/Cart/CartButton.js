02055a6716bad9db8191902473d10f5e3042bacc
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-19 12:03:16 +0530

    dynamically render total cart component

--- food-order-app/src/Components/Cart/CartButton.js
+++ food-order-app/src/Components/Cart/CartButton.js
@@ -1,17 +1,21 @@
-import React from "react"
+import React, { useContext } from "react"
 import Button from "../UI/Button/Button"
 import shoppingCart from "../../assets/images/shopping-cart.png"
-
+import CartContext from "../../store/cart-context"
 import "./CartButton.css"
 
 function CartButton(props) {
+  const cartCxt = useContext(CartContext)
+  const totalCartItem = cartCxt.items.reduce((cur, item) => {
+    return cur + item.quantity
+  }, 0)
   return (
     <Button onClick={props.onClick} className="btn cart-button">
       <span className="cart-logo">
         <img src={shoppingCart} />
       </span>
       <span className="cart-heading">Cart</span>
-      <span className="cart-total">5</span>
+      <span className="cart-total">{totalCartItem}</span>
     </Button>
   )
 }
