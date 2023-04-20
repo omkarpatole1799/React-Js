110690755f1a75a0ca911753f368d98bac562904
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-20 11:07:23 +0530

    refactoring

--- food-order-app/src/Components/Cart/CartItem.js
+++ food-order-app/src/Components/Cart/CartItem.js
@@ -1,25 +1,25 @@
 import React from "react"
 import classes from "./Cart.module.css"
+import Button from "../UI/Button/Button";
 
 function CartItem(props) {
-  return (
-    <>
-      {props.items.map((meal) => {
-        return (
-          <li key={meal.id} className={classes.mealItem}>
-            <div className="meal-info">
-              <p>{meal.mealName}</p>
-              <p>{meal.description}</p>
-              <p>$ {meal.price.toFixed(2)}</p>
+    return (<>
+        <li key={props.meal.id} className={classes.mealItem}>
+            <div className={classes.mealInfo}>
+                <span>{props.meal.mealName}</span>
+                <span>{props.meal.description}</span>
+                <span>$ {props.meal.price.toFixed(2)}</span>
             </div>
             <div className={classes.mealItemQuantityTotal}>
-              <span>Quantity</span>
-              <span> x {meal.quantity} </span>
+                <span>Quantity</span>
+                <span> x {props.meal.quantity} </span>
             </div>
-          </li>
-        )
-      })}
-    </>
-  )
+            <div className={classes.mealTotal}>
+                <span>Total amount</span>
+                <span>$ {(props.meal.price * props.meal.quantity).toFixed(2)}</span>
+            </div>
+        </li>
+    </>)
 }
+
 export default CartItem
