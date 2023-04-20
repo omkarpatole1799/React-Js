a5146c64cb87f37eac31a2b54f2a0e4ce0f6206c
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-20 11:06:34 +0530

    cart Items added

--- food-order-app/src/Components/Cart/Cart.js
+++ food-order-app/src/Components/Cart/Cart.js
@@ -1,40 +1,76 @@
-import React from "react"
+import React, {useContext, useState} from "react"
 import Button from "../UI/Button/Button"
 import Modal from "../UI/Modal"
 import classes from "./Cart.module.css"
 import CartItem from "./CartItem"
-function Cart(props) {
-  const mealData = [
-    {
-      id: "m1",
-      mealName: "Tomato",
-      price: 12.99,
-      quantity: "2",
-      amount: 25.0,
-    },
-  ]
+import CartContext from "../../store/cart-context";
 
-  return (
-    <Modal onClick={props.onClick}>
-      <ul className={classes.cart}>
-        <div className={classes.cartItem}>
-          <CartItem items={mealData} />
-        </div>
+function Cart(props) {
+    const [isCartEmpty, setIsCartEmpty] = useState(false)
+    const cartCtx = useContext(CartContext)
+
+    const totalAmount = cartCtx.items.map((item) => {
+        return item.quantity * item.price
+    }).reduce((cur, el) => {
+        return cur + el
+    }, 0)
+
+    const emptyCart = () => {
+        return <span>Empty Cart! Shop too see in cart </span>
+    }
+    const cartNotEmpty = () => {
+        return <>
+            <span>Total amount</span> <span>$ {totalAmount}</span>
+        </>
+    }
+
+    return (<Modal onClick={props.onClick}>
+        <ul className={classes.cart}>
+            <div className={classes.cartItem}>
+                {cartCtx.items.map((item) => {
+                    return <CartItem key={item.id} onClick={props.onClick} meal={item}/>
+                })}
+            </div>
+        </ul>
+
         <div className={classes.cartSummary}>
-          <div className={classes.cartTotal}>
-            <span>Total amount</span>
-            <span>$ 25.99</span>
-          </div>
-          <div className={classes.cartButton}>
-            <Button onClick={props.onClick} className={classes.cartCloseButton}>
-              Close
-            </Button>
-            <Button className={classes.cartOrderButton}>Order</Button>
-          </div>
+            <div className={classes.cartTotal}>
+                {(totalAmount !== 0) ? cartNotEmpty() : emptyCart()}
+
+            </div>
+
+            <div className={classes.cartButton}>
+                <Button onClick={props.onClick} className={classes.cartCloseButton}>
+                    Close
+                </Button>
+                {totalAmount !== 0 && <Button className={classes.cartOrderButton}>Order</Button>}
+            </div>
         </div>
-      </ul>
-    </Modal>
-  )
+
+
+        {/*{totalAmount !== 0 && <div className={classes.cartSummary}>*/}
+        {/*    <div className={classes.cartTotal}>*/}
+        {/*        <span>Total amount</span>*/}
+        {/*        <span>$ {totalAmount}</span>*/}
+        {/*    </div>*/}
+
+        {/*    <div className={classes.cartButton}>*/}
+        {/*        <Button onClick={props.onClick} className={classes.cartCloseButton}>*/}
+        {/*            Close*/}
+        {/*        </Button>*/}
+        {/*        <Button className={classes.cartOrderButton}>Order</Button>*/}
+        {/*    </div>*/}
+        {/*</div>}*/}
+
+        {/*{totalAmount === 0 && <div className={classes.cartSummary}>*/}
+        {/*    <div className={classes.cartTotal}>*/}
+        {/*        <span>Empty Cart, Shop items too see in cart</span>*/}
+        {/*    </div>*/}
+        {/*    <div className={classes.cartButton}>*/}
+        {/*        <Button onClick={props.onClick} className={classes.cartCloseButton}>Close </Button>*/}
+        {/*    </div>*/}
+        {/*</div>}*/}
+    </Modal>)
 }
 
 export default Cart
