de451cd123c4562fb36a643ff4c7a1cb404539d9
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-20 11:07:44 +0530

    if item is already present the update it

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,47 +1,51 @@
-import React, { useReducer } from "react"
+import React, {useReducer} from "react"
 import CartContext from "./cart-context"
 
 const defaultCartState = {
-  items: [],
-  totalCartItem: 0,
+    items: [],
 }
-
 const cartReducer = (state, action) => {
-  if (action.type === "ADD_ITEM") {
-    const cartItems = state.items.concat(action.item)
-    const cartItemTotal = action.item.quantity
-    console.log(cartItems)
-    return {
-      items: cartItems,
-      totalCartItem: cartItemTotal,
+    if (action.type === 'ADD_ITEM') {
+        const exsistingItemIndex = state.items.findIndex((item) => {
+            return item.id === action.item.id
+        })
+        const exsistingCartItem = state.items[exsistingItemIndex]
+
+        let cartItems
+        if (exsistingCartItem) {
+            const updatedItem = {
+                ...exsistingCartItem,
+                quantity: (exsistingCartItem.quantity + action.item.quantity)
+            }
+            cartItems = [...state.items]
+            cartItems[exsistingItemIndex] = updatedItem
+        } else {
+            cartItems = state.items.concat(action.item)
+        }
+
+        return {
+            items: cartItems
+        }
     }
-  }
-  return defaultCartState
+    return defaultCartState
 }
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCartAction] = useReducer(
-    cartReducer,
-    defaultCartState
-  )
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
-  const addItemHandler = (item) => {
-    dispatchCartAction({ type: "ADD_ITEM", item: item })
-  }
-  const removeItemHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE_ITEM", id: id })
-  }
-  const cartContext = {
-    items: cartState.items,
-    totalAmount: cartState.totalCartItem,
-    addItem: addItemHandler,
-    removeItem: removeItemHandler,
-  }
+    const addItemHandler = (item) => {
+        dispatchCartAction({type: "ADD_ITEM", item: item})
+    }
+    const removeItemHandler = (id) => {
+        dispatchCartAction({type: "REMOVE_ITEM", id: id})
+    }
 
-  return (
-    <CartContext.Provider value={cartContext}>
-      {props.children}
-    </CartContext.Provider>
-  )
+    const cartContext = {
+        items: cartState.items, addItem: addItemHandler, removeItem: removeItemHandler,
+    }
+
+    return (<CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>)
 }
 export default CartProvider
