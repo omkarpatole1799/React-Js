1623f84a0306232fe6a1e043a2ed779b78896771
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-21 10:52:31 +0530

    refactoring

--- food-order-app/src/Components/Meals/MealsList.js
+++ food-order-app/src/Components/Meals/MealsList.js
@@ -1,30 +1,33 @@
 import React from "react"
 import Input from "../UI/Input"
 import "./MealList.css"
+
 function MealsList(props) {
-  return (
-    <>
-      {props.meals.map((meal) => (
-        <li key={meal.id} className="meal-item">
-          <div className="meal-info">
-            <p>{meal.mealName}</p>
-            <p>{meal.description}</p>
-            <p>$ {meal.price.toFixed(2)}</p>
-          </div>
-          <div className="meal-input">
-            <Input
-              label={"Quantity"}
-              input={{
-                id: meal.id,
-                type: "number",
-              }}
-              meal={meal}
-            ></Input>
-          </div>
-        </li>
-      ))}
-    </>
-  )
+    return (
+        <>
+            {props.meals.map((meal) => (
+                <li key={meal.id} className="meal-item">
+                    <div className="meal-info">
+                        <p>{meal.mealName}</p>
+                        <p>{meal.description}</p>
+                        <p>$ {meal.price.toFixed(2)}</p>
+                    </div>
+                    <div className="meal-input">
+                        <Input
+                            label={"Quantity"}
+                            input={{
+                                id: meal.id,
+                                min: '1',
+                                max: '5',
+                                type: "number",
+                            }}
+                            meal={meal}
+                        ></Input>
+                    </div>
+                </li>
+            ))}
+        </>
+    )
 }
 
 export default MealsList
