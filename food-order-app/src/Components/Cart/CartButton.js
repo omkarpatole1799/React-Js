90ee1e072799d07b748e48980712c82fb8d7f2da
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-21 10:50:24 +0530

    refactoring

--- food-order-app/src/Components/Cart/CartButton.js
+++ food-order-app/src/Components/Cart/CartButton.js
@@ -1,23 +1,21 @@
-import React, { useContext } from "react"
+import React, {useContext} from "react"
 import Button from "../UI/Button/Button"
-import shoppingCart from "../../assets/images/shopping-cart.png"
 import CartContext from "../../store/cart-context"
 import "./CartButton.css"
 
 function CartButton(props) {
-  const cartCxt = useContext(CartContext)
-  const totalCartItem = cartCxt.items.reduce((cur, item) => {
-    return cur + item.quantity
-  }, 0)
-  return (
-    <Button onClick={props.onClick} className="btn cart-button">
+    const cartCxt = useContext(CartContext)
+    const totalCartItem = cartCxt.items.reduce((cur, item) => {
+        return cur + item.quantity
+    }, 0)
+
+    return (<Button onClick={props.onClick} className="cart-button">
       <span className="cart-logo">
-        <img src={shoppingCart} />
+        <i className="fa-solid fa-cart-arrow-down"></i>
       </span>
-      <span className="cart-heading">Cart</span>
-      <span className="cart-total">{totalCartItem}</span>
-    </Button>
-  )
+        <span className="cart-btn-heading">Cart</span>
+        <span className="cart-total">{totalCartItem}</span>
+    </Button>)
 }
 
 export default CartButton
