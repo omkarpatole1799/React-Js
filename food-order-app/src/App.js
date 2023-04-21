5adb46f8c353f80f80459caa22539293a7bbfac2
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-21 10:48:27 +0530

    added footer component

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,30 +1,34 @@
-import React, { useState } from "react"
+import React, {useState} from "react"
 import "./App.css"
 import Navbar from "./Components/Header/Navbar"
 import Home from "./Components/Home/Home"
 import Meals from "./Components/Meals/Meals"
 import Cart from "./Components/Cart/Cart"
 import CartProvider from "./store/CartProvider"
+import Footer from "./Components/UI/Footer/Footer";
+
 
 function App() {
-  const [modal, setModal] = useState(false)
-  const cartOpenHandler = () => {
-    setModal(true)
-  }
-  const cartCloseHandler = () => {
-    setModal(false)
-  }
-  return (
-    <CartProvider>
-      {modal && <Cart onClick={cartCloseHandler} />}
+    const [modal, setModal] = useState(false)
+    const cartOpenHandler = () => {
+        setModal(true)
+    }
+    const cartCloseHandler = () => {
+        setModal(false)
+    }
+    return (
+        <CartProvider>
+            {modal && <Cart onClick={cartCloseHandler}/>}
 
-      <Navbar onCartButtonHandler={cartOpenHandler} />
+            <Navbar onCartButtonHandler={cartOpenHandler}/>
 
-      <Home />
+            <Home/>
 
-      <Meals />
-    </CartProvider>
-  )
+            <Meals/>
+
+            <Footer/>
+        </CartProvider>
+    )
 }
 
 export default App
