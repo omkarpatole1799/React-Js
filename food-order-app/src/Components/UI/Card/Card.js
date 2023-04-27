eaa28bd43f9c3987daac0f60ff956bdad14014bb
Omkar Patole <omkarpatole1799@gmail.com>
2023-04-27 18:49:16 +0530

    removed

--- food-order-app/src/Components/UI/Card/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "./Card.css"
-function Card(props) {
-  return (
-    <div className={`card-container ${props.className}`}>{props.children}</div>
-  )
-}
-
-export default Card
