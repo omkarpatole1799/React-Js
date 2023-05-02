ce5ee7d4f7921647c9adb4f84de7bbdb2c64a4ba
Omkar Patole <omkarpatole1799@gmail.com>
2023-05-02 09:14:24 +0530

    final

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,28 +1,11 @@
-import React, { useState } from 'react';
-import UserDetailInput from './Components/User Details Input/UserDetailInput';
-import Card from './Components/UI/Card';
-import Modal from './Components/UI/Modal';
-import './App.css';
-import UserDetailsList from './Components/User Details List/UserDetailsList';
-
-// const DUMMY_DATA = [{ userName: 'omkar', age: 24, id: '1' }];
-
+import React from 'react'
+import Navbar from './Components/Navbar'
 function App() {
-    const [user, setUser] = useState('');
-
-    const inputHandler = (userDetails) => {
-        setUser((prevState) => {
-            return [userDetails[0], ...prevState];
-        });
-    };
-    return (
-        <>
-            <UserDetailInput onSubmit={inputHandler} />
-
-            {/* <Modal /> */}
-            <UserDetailsList data={user} />
-        </>
-    );
+  return (
+      <>
+          <Navbar />
+      </>
+  )
 }
 
-export default App;
+export default App
