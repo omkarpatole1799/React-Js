19f365d1e61ae1bfea91b67d13e3801885d23fd4
Omkar Patole <omkarpatole1799@gmail.com>
2023-05-07 10:36:01 +0530

    privided redux store to app component

--- redux/counter-practice-app-redux/src/components/Counter.js
+++ redux/counter-practice-app-redux/src/components/Counter.js
@@ -1,13 +1,28 @@
+import { useSelector, useDispatch } from 'react-redux'
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
+
+  const incrementHandler = () => {
+    dispatch({type: 'increment'})
+  }
+
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'})
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.value}>{counter}</div>
+      <button className='border-3 bg-blue-500 text-white p-1 m-2' onClick={incrementHandler} >Increment</button>
+      <button className='border-3 bg-blue-500 text-white p-1 m-2' onClick={decrementHandler}>Decrement</button>
+      <button className='border-3 bg-blue-500 text-white p-1 m-2' onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
