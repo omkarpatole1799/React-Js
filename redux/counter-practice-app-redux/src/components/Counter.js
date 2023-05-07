3d625edc6d2e5211cc4f75e9428dc2d2261f553a
Omkar Patole <omkarpatole1799@gmail.com>
2023-05-07 11:01:00 +0530

    redux

--- redux/counter-practice-app-redux/src/components/Counter.js
+++ redux/counter-practice-app-redux/src/components/Counter.js
@@ -3,6 +3,7 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const counter = useSelector(state => state.counter)
+  const showCounter = useSelector(state => state.showCounter)
   const dispatch = useDispatch()
 
 
@@ -10,17 +11,24 @@ const Counter = () => {
     dispatch({type: 'increment'})
   }
 
+  const increaseHandler = () => {
+    dispatch({type: 'increase', value: 10})
+  }
+
   const decrementHandler = () => {
     dispatch({type: 'decrement'})
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button className='border-3 bg-blue-500 text-white p-1 m-2' onClick={incrementHandler} >Increment</button>
+      <button className='border-3 bg-blue-500 text-white p-1 m-2' onClick={increaseHandler} >Increase</button>
       <button className='border-3 bg-blue-500 text-white p-1 m-2' onClick={decrementHandler}>Decrement</button>
       <button className='border-3 bg-blue-500 text-white p-1 m-2' onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
