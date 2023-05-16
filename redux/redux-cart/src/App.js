4a750e3ace32c499fe8d127d4e893437e8899b7e
Omkar Patole <omkarpatole1799@gmail.com>
2023-05-16 11:32:52 +0530

    added fetch req (put cartdata to firebase backend)

--- redux/redux-cart/src/App.js
+++ redux/redux-cart/src/App.js
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
 import Cart from './Components/Cart/Cart';
 import Navbar from './Components/Navbar/Navbar';
 import ItemList from './Components/ItemList/ItemList';
+
 function App() {
-  const showCart = useSelector(state=>state.cartUI.displayCart)
+  const showCart = useSelector(state => state.cartUI.displayCart)
+  const cartData = useSelector(state => state.cartItems)
+  
+  // PUT cart-data to backend
+  useEffect(() => {
+    const fetchCartDataTimer = setTimeout(() => {
+      fetch('https://cart-data-8caf2-default-rtdb.firebaseio.com/cart-data.json', {
+        method: "PUT",
+        body: JSON.stringify(cartData)
+      })
+    }, 2000 )
+
+    // cleanup function
+    return () => {
+      clearTimeout(fetchCartDataTimer)
+    }
+  },[cartData])
+
   return (
     <>
       <Navbar></Navbar>
