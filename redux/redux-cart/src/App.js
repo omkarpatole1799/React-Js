0b9d8e63313e34765dd49d2da2d47e38624b84d2
Omkar Patole <omkarpatole1799@gmail.com>
2023-06-21 05:35:02 +0530

    init

--- redux/redux-cart/src/App.js
+++ redux/redux-cart/src/App.js
@@ -1,48 +1,52 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import Cart from './Components/Cart/Cart';
-import Navbar from './Components/Navbar/Navbar';
-import ItemList from './Components/ItemList/ItemList';
-import Notification from './Components/UI/Notification/Notification';
-import { fetchData, sendData } from './Components/Store/cartData';
+import Cart from "./Components/Cart/Cart";
+import Navbar from "./Components/Navbar/Navbar";
+import ItemList from "./Components/ItemList/ItemList";
+import Notification from "./Components/UI/Notification/Notification";
+import { fetchData, sendData } from "./Components/Store/cartData";
 
-let isInitial = true
+let isInitial = true;
 
 function App() {
-  const dispatch = useDispatch()
-  const showCart = useSelector(state => state.cartUI.displayCart)
-  const cartData = useSelector(state => state.cartItems)
-  const notification = useSelector(state => state.cartUI.notification)
+	const dispatch = useDispatch();
+	const showCart = useSelector((state) => state.cartUI.displayCart);
+	const cartData = useSelector((state) => state.cartItems);
+	const notification = useSelector((state) => state.cartUI.notification);
 
-  useEffect(() => {
-    dispatch(fetchData(cartData))
-  }, [dispatch])
+	useEffect(() => {
+		dispatch(fetchData(cartData));
+	}, [dispatch]);
 
-  useEffect(() => {
-    if (isInitial) {
-      isInitial = false
-      return
-    }
- 
-    dispatch(sendData(cartData))
-  }, [cartData, dispatch])
+	useEffect(() => {
+		if (isInitial) {
+			isInitial = false;
+			return;
+		}
 
-  return (
-    <>
-      {notification && <Notification data={{
-        status: notification.status,
-        message: notification.message,
-        description: notification.description,
-      }} />}
+		dispatch(sendData(cartData));
+	}, [cartData, dispatch]);
 
-      <Navbar></Navbar>
+	return (
+		<>
+			{notification && (
+				<Notification
+					data={{
+						status: notification.status,
+						message: notification.message,
+						description: notification.description,
+					}}
+				/>
+			)}
 
-      {showCart && <Cart />}
+			<Navbar></Navbar>
 
-      <ItemList></ItemList>
-    </>
-  );
+			{showCart && <Cart />}
+
+			<ItemList></ItemList>
+		</>
+	);
 }
 
 export default App;
--- redux/redux-cart/src/Components/Store/cartData.js
+++ redux/redux-cart/src/Components/Store/cartData.js
@@ -1,65 +1,61 @@
-import { UIActions } from "./UI-slice"
-import { cartItemAction } from "./cart-items-slice"
-import notification from "./notification-actions/notification"
-
+import { UIActions } from "./UI-slice";
+import { cartItemAction } from "./cart-items-slice";
+import notification from "./notification-actions/notification";
 
 // getting cart data from server
 export const fetchData = () => {
-    return async (dispatch) => {
-        const fetchData = async () => {
-            const response = await fetch('https://cart-data-8caf2-default-rtdb.firebaseio.com/cart-data.json')
+	return async (dispatch) => {
+		const fetchData = async () => {
+			const response = await fetch(
+				"https://cart-data-01-default-rtdb.firebaseio.com/cart-data.json"
+			);
 
-            if (!response.ok) {
-                dispatch(UIActions.showNotification(
-                    notification.error
-                ))
-            }
-            const data = response.json()
-            return data
-        }
+			if (!response.ok) {
+				dispatch(UIActions.showNotification(notification.error));
+			}
+			const data = response.json();
+			return data;
+		};
 
-        try {
-            const cartData = await fetchData()
-            console.log(cartData)
-            dispatch(cartItemAction.replaceCartData({
-                items: cartData.items || [],
-                totalCartItems: cartData.totalCartItems,
-            }))
-        } catch (error) {
-            dispatch(UIActions.showNotification(
-                notification.error
-            ))
-        }
-    }
-}
-
+		try {
+			const cartData = await fetchData();
+			console.log(cartData);
+			dispatch(
+				cartItemAction.replaceCartData({
+					items: cartData.items || [],
+					totalCartItems: cartData.totalCartItems,
+				})
+			);
+		} catch (error) {
+			dispatch(UIActions.showNotification(notification.error));
+		}
+	};
+};
 
 // sending cart data to server
 export const sendData = (cartData) => {
-    return async (dispatch) => {
+	return async (dispatch) => {
+		dispatch(UIActions.showNotification(notification.sending));
 
-        dispatch(UIActions.showNotification(
-            notification.sending
-        ))
+		const sendData = async () => {
+			const response = await fetch(
+				"https://cart-data-01-default-rtdb.firebaseio.com/cart-data.json",
+				{
+					method: "PUT",
+					body: JSON.stringify(cartData),
+				}
+			);
 
-        const sendData = async () => {
-            const response = await fetch('https://cart-data-8caf2-default-rtdb.firebaseio.com/cart-data.json', {
-                method: "PUT",
-                body: JSON.stringify(cartData)
-            })
+			if (!response.ok) {
+				throw new Error("something went wrong");
+			}
+		};
 
-            if (!response.ok) {
-                throw new Error('something went wrong')
-            }
-        }
-
-        try {
-            await sendData()
-            dispatch(UIActions.showNotification(
-                notification.success
-            ))
-        } catch (error) {
-            dispatch(UIActions.showNotification(notification.error))
-        }
-    }
-}
+		try {
+			await sendData();
+			dispatch(UIActions.showNotification(notification.success));
+		} catch (error) {
+			dispatch(UIActions.showNotification(notification.error));
+		}
+	};
+};
