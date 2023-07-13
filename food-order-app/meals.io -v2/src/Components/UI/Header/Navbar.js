69f40012f5b726ab33ac7097995af5fe46437a57
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-13 08:47:22 +0530

    refactoring: navbar option in array and then map each item

--- food-order-app/meals.io -v2/src/Components/UI/Header/Navbar.js
+++ food-order-app/meals.io -v2/src/Components/UI/Header/Navbar.js
@@ -11,32 +11,48 @@ const Navbar = (props) => {
 		}
 		setOpen(!open)
 	}
+
+	const navBarItems = [
+		{ title: "Home", link: '/' },
+		{ title: "About", link: '/about' },
+		{ title: "Meals", link: '/meals' },
+		{ title: "Open Cart", link: '/cart' }
+	]
+
 	return (
 		<>
 			<nav className="bg-[#FFFFF] shadow-inner h-auto p-3">
 				<div className="md:flex justify-between ms-5 me-5 items-center">
-					<div>
-						<h4 className="font-bold text-2xl">meals.io</h4>
+					<div className="flex justify-between ">
+						<div>
+							<h4 className="font-bold text-2xl">meals.io</h4>
+						</div>
+
+						<div>
+							<NavLink to='/cart'>
+								<button onClick={props.onClick} className="bg-[#111827] text-[#D1D7DC] ps-3 pe-3 pt-2 pb-2 rounded-3xl">
+									<i className="fa-solid fa-cart-shopping"></i>
+								</button>
+							</NavLink>
+						</div>
+
+						<div>
+							<button onClick={navOpenHandler} className="text-2xl absolute right-8 top-3 cursor-pointer md:hidden click">
+								<i className={`${!open ? "fa-solid fa-bars" : "fa-solid fa-xmark"}`}></i>
+							</button>
+						</div>
 					</div>
-					<button onClick={navOpenHandler} className="text-2xl absolute right-8 top-3 cursor-pointer md:hidden click">
-						<i className={`${!open ? "fa-solid fa-bars" : "fa-solid fa-xmark"}`}></i>
-					</button>
 
 					<ul className={`absolute md:static right-0 top-14 ${!open ? "translate-x-full" : "translate-x-0"} flex flex-col bg-white z-20 md:z-auto md:bg-inherit w-full h-auto p-3 md:flex-row transition-all duration-400 ease-in-out`}>
-						<li className="md:ps-2 ms-5 mb-2 w-full">
-							<NavLink to='/'>Home</NavLink>
-						</li>
-						<li className="md:ps-2 ms-5 mb-2">
-							<NavLink to='/about'>About</NavLink>
-						</li>
-						<li className="md:ps-2 ms-5 mb-2">
-							<NavLink to='/meals'>Meals</NavLink>
-						</li>
-						<NavLink to='/cart'>
-							<button onClick={props.onClick} className="bg-[#111827] text-[#D1D7DC] ps-3 pe-3 pt-2 pb-2 rounded-3xl">
-								Open Cart
-							</button>
-						</NavLink>
+						{
+							navBarItems.map((el) => {
+								return <>
+									<li className="md:ps-2 ms-5 mb-2">
+										<NavLink to={el.link}>{el.title}</NavLink>
+									</li>
+								</>
+							})
+						}
 					</ul>
 				</div>
 			</nav>
