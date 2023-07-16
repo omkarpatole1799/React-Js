33c998197ca1783acf63460c1539533134f8061c
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-16 09:58:11 +0530

    Added: time read

--- text-editor/src/Components/TextEditor.js
+++ text-editor/src/Components/TextEditor.js
@@ -1,52 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 function TextEditor() {
     const [text, setText] = useState('')
-    const totalChar = () => {
-        let charCount = 0
-        let charArr = text.split("")        
-        for (let char of charArr){
-            if(char !== " "){
-                charCount = charCount + 1
-            }
-        }
-        return charCount
-    }
-    const totalWords = () => {
-        let words = 0
-        let wordsArr = text.split(' ')        
-        for (let word of wordsArr) {
-            if (word !== "") {
-                words = words + 1
-            }
-        }
-        return words
-    }
-    const timeRead = () => {
-        let time = 0
-        return time
-    }
-    const textChangeHandler = (e) => {
+    const [wordCount, setWordCount] = useState(0)
+    const [timeRead, setTimeRead] = useState(0)
+    const textChangeHandler = (e) =>{
         setText(e.target.value)
+        textOperations()
     }
+    
+    const textOperations = () => {
+        let wordCount = 0
+        const wordArr = text.split(" ")
+        for (let word of wordArr){
+            if(word !== ""){
+                wordCount = wordCount + 1
+            }
+        }
+        let readTime = Math.ceil((wordCount / 200 ))
+        setTimeRead(readTime)         
+        setWordCount(wordCount)
+    }
+    
     const options = [
         { title: 'Bold', id: 1 },
         { title: 'Italic', id: 2 },
         { title: 'Underline', id: 3 }
     ]
+    console.log(timeRead);
+    
+    // time read html
+    const timeReadHtml = timeRead  === 0 ? "0 Min" 
+                        :timeRead <= 1 ? "Less than a min" 
+                        :`${timeRead} min`
+
     return (
         <div>
             {options.map(el => {
                 return <button key={el.id}>{el.title} </button>
             })}
+
             <br></br>
             <br></br>
 
-            <textarea cols="50" rows="5" onChange={textChangeHandler}></textarea>
-            <p>Total char: {totalChar()}</p>
-            <p>Total words: {totalWords()}</p>
-            <p>Time read: {timeRead()}</p>
+            <textarea cols="50" rows="5" onChange={(textChangeHandler)}></textarea>
+            <p>Total words: {wordCount}</p>
+            <p>Time read: {timeReadHtml}</p>
         </div>
     )
 }
