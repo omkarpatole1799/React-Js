65f08df7cab7b1fa44f5d08f44ab8a06013bb47e
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-18 13:01:05 +0530

    refactoring

--- food-order-app/meals.io -v2/src/Components/Home.js
+++ food-order-app/meals.io -v2/src/Components/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <div className="absolute text-white flex flex-col items-start justify-center top-[20rem] ps-10 text-2xl">
         <div className="font-bold pb-4">ENJOY YOUR HEALTHY DELICIOUS FOOD</div>
         <p className="text-xs w-[10rem] pb-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Delivered fastly to your door step!
         </p>
         <NavLink to='/meals'>
           <Button >Order now!</Button>
