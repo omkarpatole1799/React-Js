543eac4e74aede82336102125c0953cec6615e49
Omkar Patole <omkarpatole1799@gmail.com>
2023-10-03 19:44:25 +0530

    add log and log list finalized

--- log/src/App.js
+++ log/src/App.js
@@ -1,13 +1,13 @@
 // function imports
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // component imports
-import AddUser from './Components/SignUp/AddUser';
-import AddLog from './Components/AddLog/AddLog';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Login from './Components/Login/Login';
-import LogList from './Components/LogList/LogList';
-import RootComponent from './Components/Root/RootComponent';
-import PrivateRoutes from './Components/Routes/PrivateRoutes';
+import AddUser from './Components/SignUp/AddUser'
+import AddLog from './Components/AddLog/AddLog'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Login from './Components/Login/Login'
+import LogList from './Components/LogList/LogList'
+import RootComponent from './Components/Root/RootComponent'
+import PrivateRoutes from './Components/Routes/PrivateRoutes'
 
 function App() {
     return (
@@ -19,13 +19,13 @@ function App() {
                             <Route path="/AddUser" element={<AddUser />} />
                             <Route path="/AddLog" element={<AddLog />} />
                             <Route path="/Dashboard" element={<Dashboard />} />
-                            <Route path="/LogList" element={<LogList />} />
+                            <Route path="/logList" element={<LogList />} />
                         </Route>
                     </Route>
                     <Route path="/login" element={<Login />} />
                 </Routes>
             </BrowserRouter>
         </>
-    );
+    )
 }
-export default App;
+export default App
--- log/src/Components/LogList/LogList.js
+++ log/src/Components/LogList/LogList.js
@@ -1,53 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import './LogList.css';
-
+import React, { useEffect, useState } from 'react'
+import './LogList.css'
+import { useNavigate } from 'react-router-dom'
 function LogList() {
-  const [logList, setLogList] = useState([]);
-  async function getLogList() {
-    let res = await fetch(
-      'http://localhost:4000/user/logList/' + localStorage.getItem('userId'),
-      {
-        headers: {
-          Authorization:
-            'Bearer ' +
-            localStorage.getItem('tocken') +
-            ' ' +
-            localStorage.getItem('userId'),
-        },
-      }
-    );
-    let { data } = await res.json();
-    console.log(data);
-    setLogList(data);
-  }
-  useEffect(() => {
-    getLogList();
-  });
+    const navigate = useNavigate()
+    const [logList, setLogList] = useState([])
+    useEffect(() => {
+        getLogList()
+    }, [])
+    async function getLogList() {
+        let res = await fetch('http://localhost:4000/user/logList/', {
+            headers: {
+                Authorization:
+                    'Bearer ' +
+                    localStorage.getItem('tocken') +
+                    ' ' +
+                    localStorage.getItem('userId'),
+            },
+        })
+        let { data } = await res.json()
+        console.log(data)
+        setLogList(data)
+    }
 
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>SR No</th>
-            <th>Log Description</th>
-            <th>Log Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {logList.map((el, i) => {
-            return (
-              <tr>
-                <td> {i + 1} </td>
-                <td> {el.logInfo} </td>
-                <td> </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+    const addLogButtonHandler = () => {
+        navigate('/addLog')
+    }
+    return (
+        <div>
+            <button onClick={addLogButtonHandler}>Add log</button>
+            {logList.length > 0 && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>SR No</th>
+                            <th>Log Description</th>
+                            <th>Log Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {logList.map((el, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td> {i + 1} </td>
+                                    <td> {el.logInfo} </td>
+                                    <td> {el.createdAt.split('T')[0]}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            )}
+            {logList.length == 0 && <p>No Logs Found</p>}
+        </div>
+    )
 }
 
-export default LogList;
+export default LogList
