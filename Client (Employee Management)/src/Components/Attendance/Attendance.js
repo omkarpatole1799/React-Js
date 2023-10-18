1e0ed4aff20073fb959bcaf1e7aea5932b40aa75
Omkar Patole <omkarpatole1799@gmail.com>
2023-10-18 09:17:48 +0530

    Add: location based attendance check

--- Client (Employee Management)/src/Components/Attendance/Attendance.js
+++ Client (Employee Management)/src/Components/Attendance/Attendance.js
@@ -5,39 +5,57 @@ var geolocation = require('geolocation')
 function Attendance() {
   const [text, setText] = useState('')
   const inButtonHandler = () => {
-    geolocation.getCurrentPosition(function (err, position) {
-      if (err) throw err
-    //   console.log(position)
-    })
-    setText('You are not in office.')
-    return
+    setText('setting attendance')
     navigator.geolocation.getCurrentPosition(
       (position, err) => {
-        if (position) {
-          let isInRegion = geolib.isPointInPolygon(
-            // { latitude: 19.95441, longitude: 73.771092 },  // my home coordinates
-            { latitude: 19.95441, longitude: 73.771092 },
-            [
-              { latitude: 19.97546, longitude: 73.77345 },
-              { latitude: 19.94157, longitude: 73.74152 },
-              { latitude: 19.91995, longitude: 73.79611 },
-              { latitude: 19.96239, longitude: 73.82186 }
-            ]
-          )
-
-          if (isInRegion) {
-            setText('Attendance successfully done.')
-          } else {
-            setText('You are not in office.')
-          }
-        }
-
+        setText('here')
         if (err) {
-          console.log(err)
+          setText('error')
+        }
+        if (!position) {
+          setText('no Position')
+        }
+        if (position) {
+          setText('position')
+          let { latitude, longitude } = position.coords
+          console.log(latitude, longitude)
         }
       }
     )
+    // geolocation.getCurrentPosition(function (err, position) {
+    //   if (err) {
+    //     // throw err
+    //     console.log(err)
+    //     setText(err)
+    //   }
+
+    //   if (!position) {
+    //     setText('no location')
+    //   }
+    //   if (position) {
+    //     console.log(position)
+    //     let { latitude, longitude } = position.coords
+
+    //     let isInRegion = geolib.isPointInPolygon(
+    //       // { latitude: 19.95441, longitude: 73.771092 },  // my home coordinates
+    //       { latitude, longitude },
+    //       [
+    //         { latitude: 19.97546, longitude: 73.77345 },
+    //         { latitude: 19.94157, longitude: 73.74152 },
+    //         { latitude: 19.91995, longitude: 73.79611 },
+    //         { latitude: 19.96239, longitude: 73.82186 }
+    //       ]
+    //     )
+
+    //     if (isInRegion) {
+    //       setText('Attendance successfully done.')
+    //     } else {
+    //       setText('You are not in office.')
+    //     }
+    //   }
+    // })
   }
+
   const outButtonHandler = () => {
     console.log('out button handler')
   }
@@ -54,3 +72,31 @@ function Attendance() {
 }
 
 export default Attendance
+
+// setText('You are not in office.')
+// navigator.geolocation.getCurrentPosition(
+//   (position, err) => {
+//     if (position) {
+//       let isInRegion = geolib.isPointInPolygon(
+//         // { latitude: 19.95441, longitude: 73.771092 },  // my home coordinates
+//         { latitude: 19.95441, longitude: 73.771092 },
+//         [
+//           { latitude: 19.97546, longitude: 73.77345 },
+//           { latitude: 19.94157, longitude: 73.74152 },
+//           { latitude: 19.91995, longitude: 73.79611 },
+//           { latitude: 19.96239, longitude: 73.82186 }
+//         ]
+//       )
+
+//       if (isInRegion) {
+//         setText('Attendance successfully done.')
+//       } else {
+//         setText('You are not in office.')
+//       }
+//     }
+
+//     if (err) {
+//       console.log(err)
+//     }
+//   }
+// )
