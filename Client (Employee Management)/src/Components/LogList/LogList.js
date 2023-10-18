3417a823430022ec359d0465776c2d6ac6326609
Omkar Patole <omkarpatole1799@gmail.com>
2023-10-18 09:18:26 +0530

    Add: log list view, UI changes

--- Client (Employee Management)/src/Components/LogList/LogList.js
+++ Client (Employee Management)/src/Components/LogList/LogList.js
@@ -1,58 +1,90 @@
 import React, { useEffect, useState } from 'react'
-import './LogList.css'
 import { useNavigate } from 'react-router-dom'
-function LogList() {
-    const navigate = useNavigate()
-    const [logList, setLogList] = useState([])
-    useEffect(() => {
-        getLogList()
-    }, [])
-    async function getLogList() {
-        let res = await fetch('http://localhost:4000/user/logList/', {
-            headers: {
-                Authorization:
-                    'Bearer ' +
-                    localStorage.getItem('tocken') +
-                    ' ' +
-                    localStorage.getItem('userId'),
-            },
-        })
-        let { data } = await res.json()
-        console.log(data)
-        setLogList(data)
-    }
 
-    const addLogButtonHandler = () => {
-        navigate('/addLog')
-    }
-    return (
+import { getReq } from '../../Utils/httpMethods'
+
+import './LogList.css'
+function LogList() {
+  const navigate = useNavigate()
+  const [logList, setLogList] = useState([])
+
+  useEffect(() => {
+    getLogList()
+  }, [])
+
+  async function getLogList() {
+    let data = await getReq({
+      url: 'user/logList/',
+      headers: {
+        headers: {
+          Authorization:
+            'Bearer ' +
+            localStorage.getItem('tocken') +
+            ' ' +
+            localStorage.getItem('userId')
+        }
+      }
+    })
+    console.log(data)
+    setLogList(data.data)
+  }
+
+  const addLogButtonHandler = () => navigate('/addLog')
+
+  return (
+    <div className="container-primary">
+      {/* LOG LIST NAV (SELECT MONTH, ADD LOG BTN) */}
+      <nav className="log-list-nav margin-bottom-md">
         <div>
-            <button onClick={addLogButtonHandler}>Add log</button>
-            {logList.length > 0 && (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>SR No</th>
-                            <th>Log Description</th>
-                            <th>Log Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {logList.map((el, i) => {
-                            return (
-                                <tr key={i}>
-                                    <td> {i + 1} </td>
-                                    <td> {el.logInfo} </td>
-                                    <td> {el.createdAt.split('T')[0]}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            )}
-            {logList.length == 0 && <p>No Logs Found</p>}
+          <select name="select-month" id="">
+            <option value="">oct</option>
+            <option value="">nov</option>
+            <option value="">dec</option>
+          </select>
         </div>
-    )
+        <button
+          className="button button--primary "
+          onClick={addLogButtonHandler}>
+          Add log
+        </button>
+      </nav>
+
+      {/* MAP ALL LOG */}
+      {logList && logList.length > 0 && (
+        <div>
+          {logList.map((el) => {
+            return (
+              <div className="log-card">
+                <p className="log-date">
+                  {el.createdAt.split('T')[0]}
+                </p>
+                <div className="log-text-container">
+                  <p>
+                    {el.logInfo.length > 100
+                      ? el.logInfo.slice(0, 100) + ' ...'
+                      : el.logInfo}
+                  </p>
+                  <button className="button button--log-menu">
+                    <ion-icon
+                      className="log-menu-logo"
+                      name="ellipsis-vertical-circle-outline"></ion-icon>
+                  </button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )}
+
+      {/* IF LOGS EMPTY THEN NO LOG FOUND */}
+      {logList.length === 0 && (
+        <div className="log-card">
+          <p>No Logs Found</p>
+          <p>Click add log to get started!</p>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default LogList
